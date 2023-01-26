21018d6529f3440ea5d9fdd59e29785ae93b6f12
oyuchan <oyc0401@gmail.com>
2023-01-27 08:48:33 +0900

    로그아웃

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:beontteuk/auth_check.dart';
+import 'package:beontteuk/inmat/auth/Inmat_token.dart';
 import 'package:beontteuk/inmat/auth/inmat_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'src/account/screens/login_page.dart';
 import 'src/navigation/navigation.dart';
 import 'utils/colorss.dart';
 import 'utils/letter_space_text_style.dart';
@@ -37,8 +39,20 @@ class MyApp extends StatelessWidget {
         ),
         // backgroundColor: Colors.white,
       ),
-      home: const AuthCheck(),
+      // home: const AuthCheck(),
+      home:  productMode(),
     );
+  }
+
+
+  Widget productMode() {
+    switch (InMatAuth.instance.status) {
+      case AuthStatus.user:
+        return const NavigatePage();
+      case AuthStatus.guest:
+      case AuthStatus.reSignIn:
+        return const LoginPage();
+    }
   }
 }
 
--- lib/src/profile/screens/profile.dart
+++ lib/src/profile/screens/profile.dart
@@ -1,4 +1,9 @@
+import 'package:beontteuk/inmat/auth/inmat_auth.dart';
+import 'package:beontteuk/src/account/screens/login_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../navigation/navigation.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
@@ -7,27 +12,43 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Stack(
+      body: Column(
         children: [
-          Container(
-            width: 200,
-            height: 200,
-            child: Image.network(
-                'https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E',
-                fit: BoxFit.cover),
+          Stack(
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                child: Image.network(
+                    'https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E',
+                    fit: BoxFit.cover),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 20,
+                child: Material(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(100),
+                  child: IconButton(
+                      onPressed: () {
+                        print('dsa');
+                      },
+                      icon: Icon(Icons.add)),
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: Material(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(100),
-              child: IconButton(
-                  onPressed: () {
-                    print('dsa');
-                  },
-                  icon: Icon(Icons.add)),
-            ),
+
+
+          CupertinoButton(
+            color: Colors.redAccent,
+            child: const Text("로그아웃"),
+            onPressed: () {
+              InMatAuth.instance.signOut();
+              Navigator.pushAndRemoveUntil(context,
+                  CupertinoPageRoute(builder: (context) => LoginPage()),(route) => false,);
+
+            },
           ),
         ],
       ),
